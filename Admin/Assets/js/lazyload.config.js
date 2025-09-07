71d139b70824230cb6161be834d982b535aee0a8
zantiizea <zantiizea@mail.ru>
2025-09-07 09:21:55 -0700

    knight

---
+++ Admin/Assets/js/lazyload.config.js
@@ -0,0 +1,158 @@
+// lazyload config
+var MODULE_CONFIG = {
+    chat:           [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/libs/notie/dist/notie.min.js',
+                      base_url + '/Admin/Assets/js/plugins/notie.js',
+                      base_url + '/Admin/Assets/js/app/chat.js'
+                    ],
+    mail:           [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/libs/notie/dist/notie.min.js',
+                      base_url + '/Admin/Assets/js/plugins/notie.js',
+                      base_url + '/Admin/Assets/js/app/mail.js'
+                    ],
+    user:           [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/libs/notie/dist/notie.min.js',
+                      base_url + '/Admin/Assets/js/plugins/notie.js',
+                      base_url + '/Admin/Assets/js/app/user.js'
+                    ],
+    search:         [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/js/app/search.js'
+                    ],
+    invoice:        [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/libs/notie/dist/notie.min.js',
+                      base_url + '/Admin/Assets/js/app/invoice.js'
+                    ],
+    musicapp:       [
+                      base_url + '/Admin/Assets/libs/list.js/dist/list.js',
+                      base_url + '/Admin/Assets/js/plugins/musicapp.js'
+                    ],
+    fullscreen:     [
+                      base_url + '/Admin/Assets/libs/jquery-fullscreen-plugin/jquery.fullscreen-min.js',
+                      base_url + '/Admin/Assets/js/plugins/fullscreen.js'
+                    ],
+    jscroll:        [
+                      base_url + '/Admin/Assets/libs/jscroll/dist/jquery.jscroll.min.js'
+                    ],
+    countTo:        [
+                      base_url + '/Admin/Assets/libs/jquery-countto/jquery.countTo.js'
+                    ],
+    stick_in_parent:[
+                      base_url + '/Admin/Assets/libs/sticky-kit/dist/sticky-kit.min.js'
+                    ],
+    stellar:        [
+                      base_url + '/Admin/Assets/libs/jquery.stellar/jquery.stellar.min.js',
+                      base_url + '/Admin/Assets/js/plugins/stellar.js'
+                    ],
+    masonry:        [
+                      base_url + '/Admin/Assets/libs/masonry-layout/dist/masonry.pkgd.min.js'
+                    ],
+    slick:          [
+                      base_url + '/Admin/Assets/libs/slick-carousel/slick/slick.css',
+                      base_url + '/Admin/Assets/libs/slick-carousel/slick/slick-theme.css',
+                      base_url + '/Admin/Assets/libs/slick-carousel/slick/slick.min.js'
+                    ],
+    sort:           [
+                      base_url + '/Admin/Assets/libs/html5sortable/dist/html.sortable.min.js',
+                      base_url + '/Admin/Assets/js/plugins/sort.js'
+                    ],
+    apexcharts:     [
+                      base_url + '/Admin/Assets/libs/apexcharts/dist/apexcharts.min.js',
+                      base_url + '/Admin/Assets/js/plugins/apexcharts.js'
+                    ],
+    chartjs:        [
+                      base_url + '/Admin/Assets/libs/moment/min/moment-with-locales.min.js',
+                      base_url + '/Admin/Assets/libs/chart.js/dist/Chart.min.js',
+                      base_url + '/Admin/Assets/libs/chart.js/dist/chart.ext.js',
+                      base_url + '/Admin/Assets/js/plugins/chartjs.js'
+                    ],
+    chartist:       [
+                      base_url + '/Admin/Assets/libs/chartist/dist/chartist.min.css',
+                      base_url + '/Admin/Assets/libs/chartist/dist/chartist.min.js',
+                      base_url + '/Admin/Assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js',
+                      base_url + '/Admin/Assets/libs/chartist/dist/chartist.ext.js',
+                      base_url + '/Admin/Assets/js/plugins/chartist.js'
+                    ],
+    dataTable:      [
+                      base_url + '/Admin/Assets/libs/datatables/media/js/jquery.dataTables.min.js',
+                      base_url + '/Admin/Assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js',
+                      base_url + '/Admin/Assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css',
+                      base_url + '/Admin/Assets/js/plugins/datatable.js'
+                    ],
+    bootstrapTable: [
+                      base_url + '/Admin/Assets/libs/bootstrap-table/dist/bootstrap-table.min.js',
+                      base_url + '/Admin/Assets/libs/bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js',
+                      base_url + '/Admin/Assets/libs/bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js',
+                      base_url + '/Admin/Assets/js/plugins/tableExport.min.js',
+                      base_url + '/Admin/Assets/js/plugins/bootstrap-table.js'
+                    ],
+    bootstrapWizard:[
+                      base_url + '/Admin/Assets/libs/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js'
+                    ],
+    dropzone:       [
+                      base_url + '/Admin/Assets/libs/dropzone/dist/min/dropzone.min.js',
+                      base_url + '/Admin/Assets/libs/dropzone/dist/min/dropzone.min.css'
+                    ],
+    typeahead:      [
+                      base_url + '/Admin/Assets/libs/typeahead.js/dist/typeahead.bundle.min.js',
+                      base_url + '/Admin/Assets/js/plugins/typeahead.js'
+                    ],
+    datepicker:     [
+                      base_url + '/Admin/Assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js',
+                      base_url + '/Admin/Assets/libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css',
+                    ],
+    daterangepicker:[
+                      base_url + '/Admin/Assets/libs/daterangepicker/daterangepicker.css',
+                      base_url + '/Admin/Assets/libs/moment/min/moment-with-locales.min.js',
+                      base_url + '/Admin/Assets/libs/daterangepicker/daterangepicker.js'
+                    ],
+    fullCalendar:   [
+                      base_url + '/Admin/Assets/libs/moment/min/moment-with-locales.min.js',
+                      base_url + '/Admin/Assets/libs/fullcalendar/dist/fullcalendar.min.js',
+                      base_url + '/Admin/Assets/libs/fullcalendar/dist/fullcalendar.min.css',
+                      base_url + '/Admin/Assets/libs/notie/dist/notie.min.js',
+                      base_url + '/Admin/Assets/js/plugins/notie.js',
+                      base_url + '/Admin/Assets/js/app/calendar.js'
+                    ],
+    parsley:        [
+                      base_url + '/Admin/Assets/libs/parsleyjs/dist/parsley.min.js'
+                    ],
+    select2:        [
+                      base_url + '/Admin/Assets/libs/select2/dist/css/select2.min.css',
+                      base_url + '/Admin/Assets/libs/select2/dist/js/select2.min.js',
+                      base_url + '/Admin/Assets/js/plugins/select2.js'
+                    ],
+    summernote:     [
+                      base_url + '/Admin/Assets/libs/summernote/dist/summernote.css',
+                      base_url + '/Admin/Assets/libs/summernote/dist/summernote-bs4.css',
+                      base_url + '/Admin/Assets/libs/summernote/dist/summernote.min.js',
+                      base_url + '/Admin/Assets/libs/summernote/dist/summernote-bs4.min.js'
+                    ],
+    vectorMap:      [
+                      base_url + '/Admin/Assets/libs/jqvmap/dist/jqvmap.min.css',
+                      base_url + '/Admin/Assets/libs/jqvmap/dist/jquery.vmap.js',
+                      base_url + '/Admin/Assets/libs/jqvmap/dist/maps/jquery.vmap.world.js',
+                      base_url + '/Admin/Assets/libs/jqvmap/dist/maps/jquery.vmap.usa.js',
+                      base_url + '/Admin/Assets/libs/jqvmap/dist/maps/jquery.vmap.france.js',
+                      base_url + '/Admin/Assets/js/plugins/jqvmap.js'
+                    ],
+    plyr:           [
+                      base_url + '/Admin/Assets/libs/plyrist/src/plyrist.css',
+                      base_url + '/Admin/Assets/libs/plyr/dist/plyr.polyfilled.min.js',
+                      base_url + '/Admin/Assets/libs/wavesurfer.js/dist/wavesurfer.min.js',
+                      base_url + '/Admin/Assets/libs/plyrist/src/plyrist.js',
+                      base_url + '/Admin/Assets/js/plugins/plyr.js'
+                    ]
+  };
+
+var MODULE_OPTION_CONFIG = {
+  parsley: {
+    errorClass: 'is-invalid',
+    successClass: 'is-valid',
+    errorsWrapper: '<ul class="list-unstyled text-sm mt-1 text-muted"></ul>'
+  }
+}
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    define( "KOONTOOLS", "https://koon.tools/" );
+
+    include __DIR__ . '/Core/KoOn.Tools.php';
